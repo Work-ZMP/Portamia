368aa1f194d5459de6facf096d8a223a967159e1
Ruslan-Alibaev <105104259+Ruslan-Alibaev@users.noreply.github.com>
2023-10-06 14:17:09 +0500

    добавил медиа запросы

--- templates/catalog.php
+++ templates/catalog.php
@@ -21,25 +21,25 @@ Template Name: Каталог
                 <a class="footer__top-btn" data-fancybox="" data-src="#popup__form">оставить заявку</a>
             </div>
             <div class="production__items">
-                <a href="#">
+                <a class="production__link" href="#">
                     <div class="production__item item1">
                         <h3 class="production__subtitle">Квартирные двери</h3>
                         <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/apartment_doors.svg" alt="">
                     </div>
                 </a>
-                <a href="#">
+                <a class="production__link" href="#">
                     <div class="production__item item2">
                         <h3 class="production__subtitle">Коттеджные двери</h3>
                         <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/cottage_doors.svg" alt="">
                     </div>
                 </a>
-                <a href="#">
+                <a class="production__link" href="#">
                     <div class="production__item item3">
                         <h3 class="production__subtitle">Межкомнатные двери</h3>
                         <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/interior_doors.svg" alt="">
                     </div>
                 </a>
-                <a href="#">
+                <a class="production__link" href="#">
                     <div class="production__item item4">
                         <h3 class="production__subtitle">Аллюминиевые перегородки</h3>
                         <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/aluminum_partitions.svg" alt="">
