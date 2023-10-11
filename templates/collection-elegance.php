c537b5aa845ccc23b349953cbb0081d854f7fd1c
Alexandr <fokin.alek@mail.ru>
2023-10-11 12:07:28 +0500

    -

--- templates/collection-elegance.php
+++ templates/collection-elegance.php
@@ -13,8 +13,8 @@ Template Name: Коллекция Elegance
     <div class="container">
         <div class="elegance__inner">
             <div class="elegance__items">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/collection-elegance/elegance_photo1.png" alt="">
-                <img src="<?php bloginfo('template_url'); ?>/assets/img/collection-elegance/elegance_photo2.png" alt="">
+                <img src="<?php the_field('img-1') ?>" alt="">
+                <img src="<?php the_field('img-2') ?>" alt="">
             </div>
         </div>
     </div>
@@ -25,13 +25,12 @@ Template Name: Коллекция Elegance
 <section class="style__section">
     <div class="container">
         <div class="style__inner">
-            <h1 class="style__title title-default">“Сочетание изысканного стиля и простоты”</h1>
-            <p class="style__descr descr-default">Переосмысление роскоши и элегантности - коллекция дверей “New Elegance” воплощает современные тренды и вдохновляет создать стильную и привлекательную входную зону для Вашего дома</p>
-            <img src="<?php bloginfo('template_url'); ?>/assets/img/collection-elegance/elegance_photo3.png" alt="">
+            <h1 class="style__title title-default"><?php the_field('style-title') ?></h1>
+            <p class="style__descr descr-default"><?php the_field('style-descr') ?></p>
+            <img src="<?php the_field('style-img') ?>" alt="">
         </div>
     </div>
 </section>
-
 
 
 <section class="catalogue__section">
@@ -41,94 +40,39 @@ Template Name: Коллекция Elegance
             <h2 class="catalogue__title">Двери коллекции</h2>
             <p class="catalogue__descr">Наши металлические двери – надежная защита для вашего дома или офиса.</p>
             <div class="catalogue__items">
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-1.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">FLEXO</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">FLEXO</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-2.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">CREDO V</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">CREDO V</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-3.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">CREDO 2</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">CREDO 2</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-4.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">MAIN</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">MAIN</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-5.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">VIRGIN</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">VIRGIN</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-6.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">GRAPHIC</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">GRAPHIC</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-7.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">LINEA</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">LINEA</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
-                <div class="catalogue__item">
-                    <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/interior/cat-item-8.png" alt="" class="catalogue__item-img">
-                        <div class="catalogue__item-top-cover">SENTYS</div>
-                    </div>
-                    <div class="catalogue__item-bottom">
-                        <div class="catalogue__item-subtitle">Модель</div>
-                        <div class="catalogue__item-title">SENTYS</div>
-                        <a href="<?php echo get_permalink(33); ?>" class="catalogue__btn">подробнее</a>
-                    </div>
-                </div>
+
+                <?php
+                global $post;
+
+                $myposts = get_posts([
+                    'include' => the_field('posts-id'),
+                    'order' => 'DESC',
+                    'orderby' => 'post_in',
+                    'post_type' => 'any'
+                ]);
+
+                if ($myposts) {
+                    foreach ($myposts as $post) {
+                        setup_postdata($post);
+                ?>
+
+                        <div class="catalogue__item">
+                            <div class="catalogue__item-top">
+                                <img src="<?php the_field('colors-img-2') ?>" alt="" class="catalogue__item-img">
+                                <div class="catalogue__item-top-cover"><?php the_field('door-name') ?></div>
+                            </div>
+                            <div class="catalogue__item-bottom">
+                                <div class="catalogue__item-subtitle">Модель</div>
+                                <div class="catalogue__item-title"><?php the_field('door-name') ?></div>
+                                <a href="<?php echo get_permalink() ?>" class="catalogue__btn">подробнее</a>
+                            </div>
+                        </div>
+
+
+                <?php }
+                }
+                wp_reset_postdata(); ?>
+
             </div>
             <hr class="catalogue__line long-line">
         </div>
@@ -147,7 +91,7 @@ Template Name: Коллекция Elegance
                         <?php echo do_shortcode('[contact-form-7 id="124b8b2" title="Оставить заявку на звонок ( контакты, дилерам)"]') ?>
                     </div>
                 </div>
-            </div>            
+            </div>
         </div>
     </div>
 </section>
