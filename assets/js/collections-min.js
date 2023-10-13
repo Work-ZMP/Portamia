05bd059b58958af38d5b02d4e7b6afb1ce5d086f
Alexandr <fokin.alek@mail.ru>
2023-10-13 10:35:34 +0500

    -

--- assets/js/collections-min.js
+++ assets/js/collections-min.js
@@ -1,1 +1,33 @@
-$(window).on("load",(function(){document.querySelectorAll(".collection__item").forEach((e=>e.addEventListener("mouseover",(function(e){$(this.querySelector(".collection__item-hide-list")).slideDown("slow")})))),document.querySelectorAll(".collection__item").forEach((e=>e.addEventListener("mouseleave",(function(e){$(this.querySelector(".collection__item-hide-list")).slideUp("slow")}))))})),document.querySelector(".collection__item-cover")&&document.querySelectorAll(".collection__item-cover").forEach((e=>""==e.textContent))&&(this.style.display="none");
+
+$(window).on('load', function () {
+
+
+document.querySelectorAll('.collection__item')
+.forEach(el => el.addEventListener('mouseover', function (e) {
+    $(this.querySelector(".collection__item-hide-list")).slideDown('slow');
+}));
+
+document.querySelectorAll('.collection__item')
+.forEach(el => el.addEventListener('mouseleave', function (e) {
+    $(this.querySelector(".collection__item-hide-list")).slideUp('slow');
+}));
+
+});
+
+if(document.querySelector('.collection__item-cover')) {
+    if (document.querySelectorAll('.collection__item-cover').forEach(b => b.textContent == ' ')) {
+        this.style.display = 'none'
+    }
+}
+
+function hideElementsWithoutText(elements) {
+    elements.forEach(element => {
+      if (!element.textContent.trim()) {
+        element.style.display = 'none';
+      }
+    });
+  }
+  
+
+  const elements = document.querySelectorAll('.collection__item-cover'); 
+  hideElementsWithoutText(elements);
--- assets/js/collections.js
+++ assets/js/collections.js
@@ -15,7 +15,19 @@ document.querySelectorAll('.collection__item')
 });
 
 if(document.querySelector('.collection__item-cover')) {
-    if (document.querySelectorAll('.collection__item-cover').forEach(b => b.textContent == '')) {
+    if (document.querySelectorAll('.collection__item-cover').forEach(b => b.textContent == ' ')) {
         this.style.display = 'none'
     }
 }
+
+function hideElementsWithoutText(elements) {
+    elements.forEach(element => {
+      if (!element.textContent.trim()) {
+        element.style.display = 'none';
+      }
+    });
+  }
+  
+
+  const elements = document.querySelectorAll('.collection__item-cover'); 
+  hideElementsWithoutText(elements);
