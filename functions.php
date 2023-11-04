c3a85fc1ef67f97a25ea558b9ab4481ee17b8ab7
Alexandr <fokin.alek@mail.ru>
2023-11-04 23:30:23 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('fancybox', "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js", array('jquery'), 'null', true);
     wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 'null', true);
     wp_enqueue_script('mainme', get_template_directory_uri() . '/assets/js/me.js', array('jquery'), 'null', true);
-    if ((is_page_template("templates/cnfg.php"))) {
+    if (is_page_template("cnfg.php")) {
         wp_enqueue_script('conf-script', get_template_directory_uri() . '/assets/js/conf-main.js', array('jquery'), 'null', true);
         wp_enqueue_script('conf-cost', get_template_directory_uri() . '/assets/js/conf-cost.js', array('jquery'), 'null', true);
     };
