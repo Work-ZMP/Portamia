d13e4ba6dc49f23e7b2478d39d22d97b91436e16
Ruslan-Alibaev <105104259+Ruslan-Alibaev@users.noreply.github.com>
2023-11-09 11:12:37 +0500

    catalog mobile

--- templates/catalog.php
+++ templates/catalog.php
@@ -21,30 +21,40 @@ Template Name: Каталог
                 <a class="footer__top-btn" data-fancybox="" data-src="#popup__form">оставить заявку</a>
             </div>
             <div class="production__items">
-                <a class="production__link" href="<?php echo get_permalink(7); ?>">
-                    <div class="production__item item1">
-                        <h3 class="production__subtitle">Квартирные двери</h3>
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/apartment_doors.svg" alt="">
+                <div class="">
+                    <div class="production__item-top">
+                        <a class="production__link" href="<?php echo get_permalink(7); ?>">
+                            <div class="production__item item1">
+                                <h3 class="production__subtitle">Квартирные двери</h3>
+                                <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/apartment_doors.svg" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
-                <a class="production__link" href="<?php echo get_permalink(33); ?>">
-                    <div class="production__item item2">
-                        <h3 class="production__subtitle">Коттеджные двери</h3>
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/cottage_doors.svg" alt="">
+                    <div class="production__item-top">
+                        <a class="production__link" href="<?php echo get_permalink(33); ?>">
+                            <div class="production__item item2">
+                                <h3 class="production__subtitle">Коттеджные двери</h3>
+                                <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/cottage_doors.svg" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
-                <a class="production__link" href="<?php echo get_permalink(35); ?>">
-                    <div class="production__item item3">
-                        <h3 class="production__subtitle">Межкомнатные двери</h3>
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/interior_doors.svg" alt="">
+                    <div class="production__item-top">
+                        <a class="production__link" href="<?php echo get_permalink(35); ?>">
+                            <div class="production__item item3">
+                                <h3 class="production__subtitle">Межкомнатные двери</h3>
+                                <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/interior_doors.svg" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
-                <a class="production__link" href="<?php echo get_permalink(37); ?>">
-                    <div class="production__item item4">
-                        <h3 class="production__subtitle">Аллюминиевые перегородки</h3>
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/aluminum_partitions.svg" alt="">
+                    <div class="production__item-top">
+                        <a class="production__link" href="<?php echo get_permalink(37); ?>">
+                            <div class="production__item item4">
+                                <h3 class="production__subtitle">Аллюминиевые перегородки</h3>
+                                <img src="<?php bloginfo('template_url'); ?>/assets/img/catalog/aluminum_partitions.svg" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
     </div>
