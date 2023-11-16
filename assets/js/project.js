b9e01e57006ffe969a2e5cfde71c1d4723df4ca3
Ruslan-Alibaev <105104259+Ruslan-Alibaev@users.noreply.github.com>
2023-11-16 15:42:33 +0500

    project mobile

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,6 +1,14 @@
 if (document.querySelector(".elegance__items")) {
     var swiperElegance = new Swiper(".elegance__items", {
-        slidesPerView: "1.5",
+        slidesPerView: "1.3",
+        spaceBetween: 15,
+        breakpoint: {
+            480 {
+                slidesPerView: "1.5",
+                spaceBetween: 30,           
+
+            }
+        },
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
