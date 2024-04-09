c3825dc2adc925e6e3553b03484d285abd67350a
Milkh <k.anosshin@bk.ru>
2024-04-09 16:24:23 +0500

    Update download_door_cfg_pdf.php

--- download_door_cfg_pdf.php
+++ download_door_cfg_pdf.php
@@ -42,7 +42,7 @@ file_put_contents($filepath2, $dompdf->output());
 //file_put_contents('../../../pdfread/'.$filename.'.pdf', $dompdf->output());
 
 header('Content-Type: application/pdf');
-header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Disposition: inline; filename="' . $filepath2 . '"');
 header('Content-Transfer-Encoding: binary');
 header('Accept-Ranges: bytes');
 
