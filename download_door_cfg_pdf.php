d481d6e0e58809a5734e72a6802dd17199d338ca
Milkh <k.anosshin@bk.ru>
2024-04-09 14:42:53 +0500

    Update download_door_cfg_pdf.php

--- download_door_cfg_pdf.php
+++ download_door_cfg_pdf.php
@@ -39,8 +39,7 @@ $dompdf->render();
 //file_put_contents('/var/www/vhosts/u0826554.plsk.regruhosting.ru/portamia.ru/pdfread/'.$filename.'.pdf', $dompdf->output());
 file_put_contents('../../../pdfread/'.$filename.'.pdf', $dompdf->output());
 $jsCode = '
-    window.open(../../../pdfread/'.$filename.'.pdf', "_blank");
-';
+    window.open(../../../pdfread/'.$filename.'.pdf, "_blank");';
 
 // Выводим JavaScript код
 echo '<script>' . $jsCode . '</script>';
