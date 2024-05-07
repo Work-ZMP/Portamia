50bb5e12f3c74e740658c68516b08132d6c3e532
Milkh <k.anosshin@bk.ru>
2024-05-07 13:42:51 +0500

    test bottom

--- get_indoor.php
+++ get_indoor.php
@@ -21,17 +21,20 @@ function merge_images($images, $proportions)
 
         $new_image[$key] = imagecreatefrompng($image);
         //test
-        $image_width = imagesx($new_image[$key]);
-        $image_height = imagesy($new_image[$key]);
+        $image_width = imagesx($new_image[$key]);//
+        $image_height = imagesy($new_image[$key]);//
 
-        $dest_x = ($x - $image_width) / 2;
-        $dest_y = ($y - $image_height) / 2;
-
+        $dest_x = ($x - $image_width) / 2;//
+        $dest_y = ($y - $image_height) / 2;//
+        $bottom_y = $y - $image_height;//
         imagealphablending($final_img, true);
         
         imagesavealpha($final_img, true);
-        imagecopy($final_img, $new_image[$key], $dest_x, $dest_y, 0, 0, $image_width, $image_height);
+        imagecopy($final_img, $new_image[$key], $dest_x, $dest_y, 0, 0, $image_width, $image_height);//
         //imagecopy($final_img, $new_image[$key], 0, 0, 0, 0, $x, $y);
+
+        // Нижнее выравнивание
+        imagecopy($final_img, $new_image[$key], $dest_x, $bottom_y, 0, 0, $image_width, $image_height);//
     }
 
     ob_start();
