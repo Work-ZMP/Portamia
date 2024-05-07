c8974f6d8b8a614de98e88857a1bba3189512e0b
Milkh <k.anosshin@bk.ru>
2024-05-07 14:29:09 +0500

    transparent?

--- get_indoor.php
+++ get_indoor.php
@@ -17,8 +17,12 @@ function merge_images($images, $proportions)
 
     $final_img = imagecreatetruecolor($x, $y);
 
-    $white = imagecolorallocate($final_img, 250, 250, 250);
-    imagefill($final_img, 0, 0, $white);
+    //$white = imagecolorallocate($final_img, 250, 250, 250);
+    //imagefill($final_img, 0, 0, $white);
+    imagealphablending($final_img, false);
+    imagesavealpha($final_img, true);
+    $transparent = imagecolorallocatealpha($final_img, 0, 0, 0, 127);
+    imagefilledrectangle($final_img, 0, 0, $x, $y, $transparent);
 
     foreach ($images as $key => $image) {
 
