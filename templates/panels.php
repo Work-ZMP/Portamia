9432f07d725c1e8bfc217276933ca7039c0642f7
Alexandr <fokin.alek@mail.ru>
2024-06-19 21:18:23 +0500

    -

--- templates/panels.php
+++ templates/panels.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Галерея
+Template Name: Панели фрезерованные
 
 */
 
@@ -20,15 +20,185 @@ Template Name: Галерея
             <div class="catalogue__items">
                 <div class="catalogue__item">
                     <div class="catalogue__item-top">
-                        <img src="<?php bloginfo('template_url'); ?>/assets/img/holding/holding-logo-portamia.svg" alt="" class="catalogue__item-img">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-0.webp" alt="" class="catalogue__item-img">
                         <div class="catalogue__item-top-cover">FK-MDF 001</div>
                     </div>
                     <div class="catalogue__item-bottom">
                         <div class="catalogue__item-subtitle">Модель</div>
                         <div class="catalogue__item-title">FK-MDF 001</div>
-                        <a href="<?php echo get_permalink() ?>" class="catalogue__btn">подробнее</a>
                     </div>
                 </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-1.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 002</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 002</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-2.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 003</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 003</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-3.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 004</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 004</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-4.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 005</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 005</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-5.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 006</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 006</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-6.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 007</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 007</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-7.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 008</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 008</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-8.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 009</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 009</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-9.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 010</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 010</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-10.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 011</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 011</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-11.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 012</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 012</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-12.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 013</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 013</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-13.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 014</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 014</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-14.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 015</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 015</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-15.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 016</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 016</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-16.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 017</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 017</div>
+                    </div>
+                </div>
+                <div class="catalogue__item">
+                    <div class="catalogue__item-top">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/panels/download-17.webp" alt="" class="catalogue__item-img">
+                        <div class="catalogue__item-top-cover">FK-MDF 018</div>
+                    </div>
+                    <div class="catalogue__item-bottom">
+                        <div class="catalogue__item-subtitle">Модель</div>
+                        <div class="catalogue__item-title">FK-MDF 018</div>
+                    </div>
+                </div>
+                
                 
             </div>
             <hr class="catalogue__line long-line">
